1f2ecf880bca0e577e92159de6f570267ddb8046
JokoN3 <jokosan97@gmail.com>
2026-03-25 17:36:09 +0100

    added printing int

--- printf.c
+++ printf.c
@@ -6,7 +6,7 @@
 /*   By: yoneshev <yoneshev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/03/17 16:00:01 by yoneshev      #+#    #+#                 */
-/*   Updated: 2026/03/20 18:36:03 by yoneshev      ########   odam.nl         */
+/*   Updated: 2026/03/25 17:32:20 by yoneshev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,60 @@ int	printf_str(char *s)
 	return (i);
 }
 
+int	printf_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	get_num_len(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n < 0)
+	{
+		n = -n;
+		i++;
+	}
+	while (n > 0)
+	{
+		i++;
+		n /= 10;
+	}
+	return (i);
+}
+
+int	printf_putnbr(int n)
+{
+	char	c;
+	int		len;
+
+	if (n == INT_MIN)
+		return (write(1, "-2147483648", 11), 11);
+	if (n == 0)
+		return (write(1, "0", 1), 1);
+	len = get_num_len(n);
+	if (n < 0)
+	{
+		n = -n;
+		write(1, "-", 1);
+	}
+	if (n >= 10)
+		printf_putnbr(n / 10);
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+	return (len);
+}
+
 int	handle_args(va_list *args, char format)
 {
 	if (format == 'c')
 		return (ft_putchar_fd(va_arg(*args, int), 1), 1);
 	if (format == 's')
 		return (printf_str(va_arg(*args, char *)));
+	if (format == 'd' || format == 'i')
+		return (printf_putnbr(va_arg(*args, int)));
 	return 0;
 }
 
@@ -54,15 +102,9 @@ int	ft_printf(const char *format, ...)
 			char_count += handle_args(&args, *format);
 		}
 		else
-			char_count++;
+			char_count += printf_putchar(*format);
 		format++;
 	}
 	va_end(args);
 	return (char_count);
-}
-
-int main()
-{
-	int x = printf("%s%c", "hello", '!');
-	printf("\n%d", x);
 }
--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: yoneshev <yoneshev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/03/17 15:59:14 by yoneshev      #+#    #+#                 */
-/*   Updated: 2026/03/17 16:04:26 by yoneshev      ########   odam.nl         */
+/*   Updated: 2026/03/25 17:14:35 by yoneshev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 
 # include "libft/libft.h"
 # include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+int	handle_args(va_list *args, char format);
+int	printf_str(char *s);
 
 #endif
